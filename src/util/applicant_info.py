9b470e2090980e3a8a3a1784f6680ae848fcda9b
Kris Taylor <kdtaylor75@gmail.com>
2023-02-08 09:17:52 -0600

    Update applicant_info.py

--- src/util/applicant_info.py
+++ src/util/applicant_info.py
@@ -1,29 +1,29 @@
 general_info = {
-    "applicant_name": "Kris Taylor",
-    "applicant_phone": "901.491.1087",
-    "applicant_email": "kris.taylor@decisionbase.io",
-    "applicant_city": "Olive Branch",
-    "applicant_state": "MS", 
-    "applicant_zipcode": "38654",
-    "applicant_linkedin": "linkedin.com/in/kris~taylor",
-    "summary": "One cool dude...",
-    "key_skills": "skill1"
+    "applicant_name": "",
+    "applicant_phone": "",
+    "applicant_email": "",
+    "applicant_city": "",
+    "applicant_state": "", 
+    "applicant_zipcode": "",
+    "applicant_linkedin": "",
+    "summary": "",
+    "key_skills": ""
 }
 
 professional_experience = [{
-    'company_name': 'Heal',
-    'company_city': 'Los Angeles',
-    'company_state': 'CA',
-    'start_date': 'August 2022',
-    'end_date': 'Present',
-    'job_title': 'Director, Engineering Logistics',
-    'job_description': 'Did stuff...'
+    'company_name': '',
+    'company_city': '',
+    'company_state': '',
+    'start_date': '',
+    'end_date': '',
+    'job_title': '',
+    'job_description': ''
 }, {
-    'company_name': 'Green Mountain Technology',
-    'company_city': 'Memphis',
-    'company_state': 'TN',
-    'start_date': 'October 2021',
-    'end_date': 'August 2022',
-    'job_title': 'Director, Data Analytics and Business Intelligence',
-    'job_description': 'Did other stuff...'
+    'company_name': '',
+    'company_city': '',
+    'company_state': '',
+    'start_date': '',
+    'end_date': '',
+    'job_title': '',
+    'job_description': ''
 }]
